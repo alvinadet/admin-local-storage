11f05894d6b67a5e9e85ed7a60bf2dec19d3e553
sodojiwo <alvinadetya@rocketmail.com>
2018-10-28 22:10:56 +0700

    dashbiard hilang

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -18,43 +18,43 @@ import {
 } from '../views';
 
 const dashboardRoutes = [
-  {
-    path: '/dashboard',
-    sidebarName: 'Dashboard',
-    navbarName: 'Material Dashboard',
-    icon: Dashboard,
-    component: DashboardPage
-  },
-  {
-    path: '/user',
-    sidebarName: 'User Profile',
-    navbarName: 'Profile',
-    icon: Person,
-    component: UserProfile
-  },
+  // {
+  //   path: '/dashboard',
+  //   sidebarName: 'Dashboard',
+  //   navbarName: 'Material Dashboard',
+  //   icon: Dashboard,
+  //   component: DashboardPage
+  // },
+  // {
+  //   path: '/user',
+  //   sidebarName: 'User Profile',
+  //   navbarName: 'Profile',
+  //   icon: Person,
+  //   component: UserProfile
+  // },
 
-  {
-    path: '/typography',
-    sidebarName: 'Typography',
-    navbarName: 'Typography',
-    icon: LibraryBooks,
-    component: Typography
-  },
-  {
-    path: '/icons',
-    sidebarName: 'Icons',
-    navbarName: 'Icons',
-    icon: BubbleChart,
-    component: Icons
-  },
+  // {
+  //   path: '/typography',
+  //   sidebarName: 'Typography',
+  //   navbarName: 'Typography',
+  //   icon: LibraryBooks,
+  //   component: Typography
+  // },
+  // {
+  //   path: '/icons',
+  //   sidebarName: 'Icons',
+  //   navbarName: 'Icons',
+  //   icon: BubbleChart,
+  //   component: Icons
+  // },
 
-  {
-    path: '/notifications',
-    sidebarName: 'Notifications',
-    navbarName: 'Notifications',
-    icon: Notifications,
-    component: NotificationsPage
-  },
+  // {
+  //   path: '/notifications',
+  //   sidebarName: 'Notifications',
+  //   navbarName: 'Notifications',
+  //   icon: Notifications,
+  //   component: NotificationsPage
+  // },
   {
     path: '/database',
     sidebarName: 'Database',
