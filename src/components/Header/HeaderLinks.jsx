5a1ab746f0e3e13285976fd3adcec1ac19740d1e
sodojiwo <alvinadetya@rocketmail.com>
2018-10-28 22:31:18 +0700

    className

--- src/components/Header/HeaderLinks.jsx
+++ src/components/Header/HeaderLinks.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import classNames from "classnames";
+import React from 'react';
+import classNames from 'classnames';
 // @material-ui/core components
-import withStyles from "@material-ui/core/styles/withStyles";
-import MenuItem from "@material-ui/core/MenuItem";
-import MenuList from "@material-ui/core/MenuList";
-import Grow from "@material-ui/core/Grow";
-import Paper from "@material-ui/core/Paper";
-import ClickAwayListener from "@material-ui/core/ClickAwayListener";
-import Hidden from "@material-ui/core/Hidden";
-import Poppers from "@material-ui/core/Popper";
+import withStyles from '@material-ui/core/styles/withStyles';
+import MenuItem from '@material-ui/core/MenuItem';
+import MenuList from '@material-ui/core/MenuList';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Hidden from '@material-ui/core/Hidden';
+import Poppers from '@material-ui/core/Popper';
 // @material-ui/icons
-import Person from "@material-ui/icons/Person";
-import Notifications from "@material-ui/icons/Notifications";
-import Dashboard from "@material-ui/icons/Dashboard";
-import Search from "@material-ui/icons/Search";
+import Person from '@material-ui/icons/Person';
+import Notifications from '@material-ui/icons/Notifications';
+import Dashboard from '@material-ui/icons/Dashboard';
+import Search from '@material-ui/icons/Search';
 // core components
-import CustomInput from "components/CustomInput/CustomInput.jsx";
-import Button from "components/CustomButtons/Button.jsx";
+import CustomInput from 'components/CustomInput/CustomInput.jsx';
+import Button from 'components/CustomButtons/Button.jsx';
 
-import headerLinksStyle from "assets/jss/material-dashboard-react/components/headerLinksStyle.jsx";
+import headerLinksStyle from 'assets/jss/material-dashboard-react/components/headerLinksStyle.jsx';
 
 class HeaderLinks extends React.Component {
   state = {
@@ -44,12 +44,12 @@ class HeaderLinks extends React.Component {
         <div className={classes.searchWrapper}>
           <CustomInput
             formControlProps={{
-              className: classes.margin + " " + classes.search
+              className: classes.margin + ' ' + classes.search
             }}
             inputProps={{
-              placeholder: "Search",
+              placeholder: 'Search',
               inputProps: {
-                "aria-label": "Search"
+                'aria-label': 'Search'
               }
             }}
           />
@@ -58,12 +58,11 @@ class HeaderLinks extends React.Component {
           </Button>
         </div>
         <Button
-          color={window.innerWidth > 959 ? "transparent" : "white"}
+          color={window.innerWidth > 959 ? 'transparent' : 'white'}
           justIcon={window.innerWidth > 959}
           simple={!(window.innerWidth > 959)}
           aria-label="Dashboard"
-          className={classes.buttonLink}
-        >
+          className={classes.buttonLink}>
           <Dashboard className={classes.icons} />
           <Hidden mdUp implementation="css">
             <p className={classes.linkText}>Dashboard</p>
@@ -74,14 +73,13 @@ class HeaderLinks extends React.Component {
             buttonRef={node => {
               this.anchorEl = node;
             }}
-            color={window.innerWidth > 959 ? "transparent" : "white"}
+            color={window.innerWidth > 959 ? 'transparent' : 'white'}
             justIcon={window.innerWidth > 959}
             simple={!(window.innerWidth > 959)}
-            aria-owns={open ? "menu-list-grow" : null}
+            aria-owns={open ? 'menu-list-grow' : null}
             aria-haspopup="true"
             onClick={this.handleToggle}
-            className={classes.buttonLink}
-          >
+            className={classes.buttonLink}>
             <Notifications className={classes.icons} />
             <span className={classes.notifications}>5</span>
             <Hidden mdUp implementation="css">
@@ -97,50 +95,43 @@ class HeaderLinks extends React.Component {
             disablePortal
             className={
               classNames({ [classes.popperClose]: !open }) +
-              " " +
+              ' ' +
               classes.pooperNav
-            }
-          >
+            }>
             {({ TransitionProps, placement }) => (
               <Grow
                 {...TransitionProps}
                 id="menu-list-grow"
                 style={{
                   transformOrigin:
-                    placement === "bottom" ? "center top" : "center bottom"
-                }}
-              >
+                    placement === 'bottom' ? 'center top' : 'center bottom'
+                }}>
                 <Paper>
                   <ClickAwayListener onClickAway={this.handleClose}>
                     <MenuList role="menu">
                       <MenuItem
                         onClick={this.handleClose}
-                        className={classes.dropdownItem}
-                      >
+                        className={classes.dropdownItem}>
                         Mike John responded to your email
                       </MenuItem>
                       <MenuItem
                         onClick={this.handleClose}
-                        className={classes.dropdownItem}
-                      >
+                        className={classes.dropdownItem}>
                         You have 5 new tasks
                       </MenuItem>
                       <MenuItem
                         onClick={this.handleClose}
-                        className={classes.dropdownItem}
-                      >
+                        className={classes.dropdownItem}>
                         You're now friend with Andrew
                       </MenuItem>
                       <MenuItem
                         onClick={this.handleClose}
-                        className={classes.dropdownItem}
-                      >
+                        className={classes.dropdownItem}>
                         Another Notification
                       </MenuItem>
                       <MenuItem
                         onClick={this.handleClose}
-                        className={classes.dropdownItem}
-                      >
+                        className={classes.dropdownItem}>
                         Another One
                       </MenuItem>
                     </MenuList>
@@ -151,12 +142,11 @@ class HeaderLinks extends React.Component {
           </Poppers>
         </div>
         <Button
-          color={window.innerWidth > 959 ? "transparent" : "white"}
+          color={window.innerWidth > 959 ? 'transparent' : 'white'}
           justIcon={window.innerWidth > 959}
           simple={!(window.innerWidth > 959)}
           aria-label="Person"
-          className={classes.buttonLink}
-        >
+          className={classes.buttonLink}>
           <Person className={classes.icons} />
           <Hidden mdUp implementation="css">
             <p className={classes.linkText}>Profile</p>
